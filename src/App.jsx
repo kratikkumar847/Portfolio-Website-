446df45a76200d06365803d5a51a1439c20278f8
kratikkumar847 <kratikkumar847@gmail.com>
2022-08-31 23:08:14 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./Components/about/About";
+import Intro from "./Components/Intro/Intro";
+
+
+const App = () => {
+  return <div>
+      <Intro />
+      <About />
+  </div>;
+};
+
+export default App;
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,33 @@
+import "./intro.css"
+import Me from "../../img/Girl.png"
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is </h2>
+                    <h1 className="i-name">Joey King</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content creater</div>
+                            <div className="i-title-item">Data scientist</div>
+                            <div className="i-title-item">Tester</div>
+                        </div>
+                    </div>
+                    <p className="i-desc">
+                        I design and develop services for customers of all size, specializing in creating stylish, mordern website, web services and online stores.
+                    </p>
+                </div>
+            </div>
+            <div className="i-right">
+                <div className="i-bg"></div>
+                <img src={Me} alt="" className="i-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Intro;
